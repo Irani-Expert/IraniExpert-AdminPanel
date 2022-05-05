2e23e348dc77302b91a4ce166ba8677305c57c12
beygiamin <96768480+beygiamin@users.noreply.github.com>
2022-05-05 15:09:46 +0430

    fixing-signUp

--- src/app/views/session/signup/signup.component.ts
+++ src/app/views/session/signup/signup.component.ts
@@ -1,4 +1,11 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ResolveEnd, ResolveStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment.prod';
+import { AuthService } from 'src/app/shared/services/auth.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  loading: boolean;
+  loadingText: string;
+  signupForm: FormGroup;
+  public form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+) { }
 
-  constructor() { }
+   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof RouteConfigLoadStart || event instanceof ResolveStart) {
+          this.loadingText = 'Loading Dashboard Module...';
 
-  ngOnInit(): void {
+          this.loading = true;
+      }
+      if (event instanceof RouteConfigLoadEnd || event instanceof ResolveEnd) {
+          this.loading = false;
+      }
+  });
+
+  this.signupForm = this.fb.group({
+      email: ['name@example.com', Validators.required],
+      password: ['1234', Validators.required]
+  });
   }
 
+
+  signup() {
+    this.loading = true;
+    this.loadingText = 'در حال ورود...';
+    // this.auth.signin(this.signinForm.value)
+    //     .subscribe(res => {
+    //         this.router.navigateByUrl('/dashboard/v1');
+    //         this.loading = false;
+    //     });
+}
 }
