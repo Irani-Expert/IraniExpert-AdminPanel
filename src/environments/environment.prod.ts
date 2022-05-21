b59dc6bd1a6b1ee5b8d3ce62b6870e19f3ed4bf2
yaser.bh1995 <yaser.bh1995@gmail.com>
2022-05-21 18:35:58 +0430

    fix upload url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
     appVersion:'1.2.41',
     apiVersion:'1.0.0'
   },
-  uploadUrl:"https://demo.iraniExpert.com/fileUploader",
+  uploadUrl:"https://iraniexpert.com/fileUploader",
   jwtToken:localStorage.getItem('jwtToken') ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidmFoaWRBaG1hZGkiLCJyb2xlIjoiQWRtaW5pc3RyYXRvciIsIm5iZiI6MTY1MTg1MzY4MCwiZXhwIjoxNjUzMDYzMjgwLCJpYXQiOjE2NTE4NTM2ODAsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjYwMDEvIn0.1kOxLOvfVW0VO9QdhWVfaJ-44fCbkjEWGFuCY9GR_Ic"
 };
