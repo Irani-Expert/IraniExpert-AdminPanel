5730acb1803801c8eda4df0e91fd54a92eafddfd
beygiamin <96768480+beygiamin@users.noreply.github.com>
2022-05-21 23:33:59 +0430

    facilitiy(almost-done)-adding-delete-faq

---
+++ src/app/views/prd/facilitiy/facilitiy.interface.ts
@@ -0,0 +1,6 @@
+import { IBase } from 'src/app/shared/interfaces/Base/base.interface';
+
+export interface IFacilitiy extends IBase {
+    productId : number;
+    product : string;
+}
--- src/app/views/prd/facilitiy/facility.service.ts
+++ src/app/views/prd/facilitiy/facility.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Result } from 'src/app/shared/models/Base/result.model';
+import { BaseService } from 'src/app/shared/services/baseService/baseService';
+import { environment } from 'src/environments/environment.prod';
+import { FacilitiyModel } from './facilitiy.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FacilityService {
+export class FacilitiyService  extends BaseService<FacilitiyModel, 0> {
+  userGuid = environment.jwtToken;
 
-  constructor() { }
+  constructor(public _http: HttpClient) {
+
+    super(_http, environment.api.baseUrl);
+   }
+
+    /**
+   * درخواست دریافت همه
+   * @param route
+   * @returns all
+   */
+     getFacilitiyByProductId(pageIndex: number, pageSize: number, pageOrder: string, filter: string, productId: number): Observable<Result<FacilitiyModel[]>> {
+      let _options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': 'bearer '+environment.jwtToken
+        }),
+      };
+      return this._http.get<Result<FacilitiyModel[]>>(this._base + "/Facilitiy/GetByProductID/" + productId +
+      "?pagIndex="+pageIndex+
+      "&pageSize="+pageSize+
+      "&pageOrder="+pageOrder+
+      "&filter="+filter+
+      "&productId="+productId,
+      _options);
+    }
 }
+
+
+
