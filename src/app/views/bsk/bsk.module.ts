ce9e9adf670c7f1bddf5ec05e259c37458202886
beygiamin <96768480+beygiamin@users.noreply.github.com>
2022-06-04 18:53:20 +0430

    making-invoice(not-started-yet)

--- src/app/views/bsk/bsk.module.ts
+++ src/app/views/bsk/bsk.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BskRoutingModule } from './bsk.routing';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { OrderComponent } from './order/order.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 @NgModule({
-  declarations: [OrderComponent],
+  declarations: [OrderComponent, InvoiceComponent],
   imports: [CommonModule, BskRoutingModule, NgxPaginationModule],
 })
 export class BskModule {}
