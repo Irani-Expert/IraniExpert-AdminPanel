4bbfb6efbadbf3c7cdcba65db422214c003f328f
Amin Beygi <96768480+beygiamin@users.noreply.github.com>
2022-07-25 13:42:24 +0430

    comment-parent-show

--- src/app/shared/services/baseService/baseService.interface.ts
+++ src/app/shared/services/baseService/baseService.interface.ts
@@ -1,19 +1,28 @@
-
 import { Observable } from 'rxjs';
 import { Filter } from '../../models/Base/filter.model';
 import { Result } from '../../models/Base/result.model';
-
 
 /**
  * اینرفیس سرویس پایه
  * @template T
  */
 export interface IBaseService<T, ID> {
-	create(t: object, route: string): Observable<Result<T>>;
-	update(id: number, t: T, route: string): Observable<Result<T>>;
-	getOneByID(id:number, route: string): Observable<Result<T>>;
-  getTitleValues(pageIndex:number,pageSize:number,pageOrder:string,filter:string,route: string): Observable<Result<T[]>>;
-	get(pageIndex:number,pageSize:number,pageOrder:string,filter:string,route: string): Observable<Result<T[]>>;
-	delete(id: number,route: string): Observable<Result<T>>;
-
+  create(t: object, route: string): Observable<Result<T>>;
+  update(id: number, t: T, route: string): Observable<Result<T>>;
+  getOneByID(id: number, route: string): Observable<Result<T>>;
+  getTitleValues(
+    pageIndex: number,
+    pageSize: number,
+    pageOrder: string,
+    filter: string,
+    route: string
+  ): Observable<Result<T[]>>;
+  get(
+    pageIndex: number,
+    pageSize: number,
+    pageOrder: string,
+    filter: string,
+    route: string
+  ): Observable<Result<T[]>>;
+  delete(id: number, route: string): Observable<Result<T>>;
 }
