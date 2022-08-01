de38a40dfc62a8fa6091546961bdb4a0c028ddaa
Amin Beygi <96768480+beygiamin@users.noreply.github.com>
2022-08-01 18:58:36 +0430

    last-push

--- src/app/shared/pipes/relative-time.pipe.ts
+++ src/app/shared/pipes/relative-time.pipe.ts
@@ -3,34 +3,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'relativeTime' })
 export class RelativeTimePipe implements PipeTransform {
   transform(value: Date) {
-    if (!(value instanceof Date)) {
-      value = new Date(value);
-    }
+    if (!(value instanceof Date)) value = new Date(value);
 
-    const seconds: number = Math.floor(
+    let seconds: number = Math.floor(
       (new Date().getTime() - value.getTime()) / 1000
     );
     let interval: number = Math.floor(seconds / 31536000);
 
     if (interval > 1) {
-      return interval + ' ثانیه قبل';
+      return interval + ' years ago';
     }
     interval = Math.floor(seconds / 2592000);
     if (interval > 1) {
-      return interval + 'ماه قبل';
+      return interval + ' months ago';
     }
     interval = Math.floor(seconds / 86400);
     if (interval > 1) {
-      return interval + 'روز قبل';
+      return interval + ' days ago';
     }
     interval = Math.floor(seconds / 3600);
     if (interval > 1) {
-      return interval + ' ساعت قبل';
+      return interval + ' hours ago';
     }
     interval = Math.floor(seconds / 60);
     if (interval > 1) {
-      return interval + ' دقیقه پیش';
+      return interval + ' minutes ago';
     }
-    return Math.floor(seconds) + 'ثانیه پیش';
+    return Math.floor(seconds) + ' seconds ago';
   }
 }
