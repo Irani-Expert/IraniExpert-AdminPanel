ebe7fb8aa59a9c5a65b5b4bd803b11168a1920db
Amin Beygi <96768480+beygiamin@users.noreply.github.com>
2022-08-15 13:03:32 +0430

    user-role-list-addOrEdit

--- src/app/views/sec/user-role/user-role.interface.ts
+++ src/app/views/sec/user-role/user-role.interface.ts
@@ -1,3 +1,10 @@
 import { IBase } from 'src/app/shared/interfaces/Base/base.interface';
 
-export interface IUserRole extends IBase {}
+export interface IUserRole extends IBase {
+  userId: number;
+  userName: string;
+  roleName: string;
+  roleId: number;
+  updateBy: number;
+  createBy: number;
+}
