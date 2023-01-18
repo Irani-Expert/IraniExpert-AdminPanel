e2b511fb6e8b6241e6c5703a27000a98c0b88e19
mohammadt <aria.persian.programer@gmail.com>
2023-01-18 16:04:45 +0330

    lastChenges

--- src/app/shared/loader.interceptor.ts
+++ src/app/shared/loader.interceptor.ts
@@ -8,10 +8,14 @@ import {
 import { Observable } from 'rxjs';
 import { delay, finalize } from 'rxjs/operators';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { UserInfoModel } from './models/userInfoModel';
+import { AuthenticateService } from './services/auth/authenticate.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
+  private _authService: AuthenticateService;
   constructor(private loader: NgxSpinnerService) {}
+
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     this.loader.show();
@@ -20,5 +24,21 @@ export class LoaderInterceptor implements HttpInterceptor {
       delay(10),
       finalize(() => this.loader.hide())
     );
+  }
+  checkUserPermission() {
+    
+    let currentUser: UserInfoModel = JSON.parse(
+      localStorage.getItem('currentUser') ?? '{}'
+    );
+    if (currentUser.token != undefined) {
+      this._authService
+        .checkUserPermission(currentUser.token)
+        .subscribe((res) => {
+          if (!res) {
+            localStorage.clear();
+            this._authService.logout()
+          }
+        });
+    }
   }
 }
--- src/app/views/shr/all-comment/all-comment.component.ts
+++ src/app/views/shr/all-comment/all-comment.component.ts
@@ -141,4 +141,7 @@ export class AllCommentComponent implements OnInit {
         }
       );
   }
+  deleteComment(){
+
+  }
 }
