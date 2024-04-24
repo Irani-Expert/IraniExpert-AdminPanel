2dfc68cd88643f984e8d2e8b179dd38059c63ac9
Amin Beygi <beygoamin@gmail.com>
2024-04-24 15:56:39 +0330

    articles

--- src/app/shared/models/Base/paginate.model.ts
+++ src/app/shared/models/Base/paginate.model.ts
@@ -1,8 +1,9 @@
-export class Paginate<T>{
-  hasNextPage:boolean;
-  hasPreviousPage:boolean;
-  items:T
-  pageNumber:number;
-  totalCount:number;
-  totalPages:number;
+export class Paginate<T> {
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+  items: T;
+  pageNumber: number;
+  pageSize?: number;
+  totalCount: number;
+  totalPages: number;
 }
---
+++ src/app/views/cnt/article/article/article-filter.model.ts
@@ -0,0 +1,5 @@
+export class ArticleFilter {
+  categories = new Array<number>();
+  rates = new Array<number>();
+  blogName = '';
+}
